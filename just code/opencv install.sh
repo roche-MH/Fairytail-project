7f240c8c3920caead0171798ca0cb8b5a192872c
roche-MH <53164586+roche-MH@users.noreply.github.com>
2021-01-28 14:56:27 +0900

    ubuntu 18.04 install opencv

---
+++ just code/opencv install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# default opencv remove
+sudo apt-get remove libopencv*
+sudo apt-get autoremove
+
+sudo apt-get update
+sudo apt-get upgrade
+
+#develop tool
+sudo apt-get install build-essential cmak unzip pkg-config -y
+
+#libraries install
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev v4l-utils libxvidcore-dev libx264-dev libxine2-dev -y
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev -y
+sudo apt-get install libgtk-3-dev -y
+sudo apt-get install mesa-utils libgl1-mesa-dri libgtkgl2.0-dev libgtkglext1-dev -y
+sudo apt-get install libatlas-base-dev gfortran libeigen3-dev -y
+sudo apt-get install python2.7-dev python3-dev python-numpy python3-numpy -y
+
+mkdir /home/superbin/opencv
+cd /home/superbin/opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.0.zip
+
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+cd opencv-3.4.0
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE 
+-D BUILD_opencv_cudacodec=OFF \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D WITH_TBB=OFF \
+-D WITH_IPP=OFF \
+-D WITH_1394=OFF \
+-D BUILD_WITH_DEBUG_INFO=OFF \
+-D BUILD_DOCS=OFF \
+-D INSTALL_C_EXAMPLES=ON \
+-D INSTALL_PYTHON_EXAMPLES=ON \
+-D BUILD_EXAMPLES=OFF \
+-D BUILD_TESTS=OFF \
+-D BUILD_PERF_TESTS=OFF \
+-D WITH_QT=OFF \
+-D WITH_GTK=ON \
+-D WITH_OPENGL=ON \
+-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.0/modules \
+-D WITH_V4L=ON  \
+-D WITH_FFMPEG=ON \
+-D WITH_XINE=ON \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
+-D PYTHON2_INCLUDE_DIR=/usr/include/python2.7 \
+-D PYTHON2_NUMPY_INCLUDE_DIRS=/usr/lib/python2.7/dist-packages/numpy/core/include/ \
+-D PYTHON2_PACKAGES_PATH=/usr/lib/python2.7/dist-packages \
+-D PYTHON2_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so \
+-D PYTHON3_INCLUDE_DIR=/usr/include/python3.6m \
+-D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/lib/python3/dist-packages/numpy/core/include/  \
+-D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+-D PYTHON3_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so \
+../
+CPUCOUNT=`cat /proc/cpuinfo | grep processor | wc -l`
+make -j{$CPUCOUNT}
+
+sudo make install
+sudo sh -c echo '/usr/local/lib/' > sudo /etc/ld.so.conf.d/opencv.conf
+sudo ldconfig
